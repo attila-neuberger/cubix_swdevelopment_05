53ced2c84e21292f162f467a19c5709432767604
Attila Neuberger <neuberger.attila@intuiware.hu>
2026-05-13 00:15:06 +0200

    Initial commit

---
+++ src/main/java/hu/neuberger/base_math/BaseMath.java
@@ -0,0 +1,8 @@
+package hu.neuberger.base_math;
+
+public class BaseMath {
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+}
---
+++ src/test/java/hu/neuberger/base_math/BaseMathTest.java
@@ -0,0 +1,13 @@
+package hu.neuberger.base_math;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BaseMathTest {
+
+	@Test
+	public void testAddPositive() {
+		assertEquals(5, BaseMath.add(2, 3));
+	}
+}
